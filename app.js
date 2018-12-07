a3f6b4d8174d0ecc8f30b9f3be544d1f6873337d
Ayush Goyal <ayush987goyal@gmail.com>
2018-12-07 23:19:10 +0530

    Add basic gql

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Hi', 'there', 'yo!'];
+      },
+      createEvent: args => {
+        return args.name;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000/'));
