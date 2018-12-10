34f804fd7690eadd62ec8ecec4270b5f2a89976e
Ayush Goyal <ayush987goyal@gmail.com>
2018-12-10 22:31:45 +0530

    Add custom types and update accordingly

--- app.js
+++ app.js
@@ -5,18 +5,35 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   '/graphql',
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput!): Event
       }
 
       schema {
@@ -26,10 +43,18 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Hi', 'there', 'yo!'];
+        return events;
       },
-      createEvent: args => {
-        return args.name;
+      createEvent: ({ eventInput: { title, description, price, date } }) => {
+        const event = {
+          _id: Math.random().toString(),
+          title,
+          description,
+          price: +price,
+          date
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
