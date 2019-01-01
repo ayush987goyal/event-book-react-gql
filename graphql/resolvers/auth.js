9ce73f57c2d4ee6f68cef79711ce6d66140be0a1
Ayush Goyal <ayush987goyal@gmail.com>
2019-01-01 21:47:26 +0530

    Add login query

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
 
@@ -14,6 +15,27 @@ module.exports = {
       const user = new User({ email, password: hashedPassword });
       const savedUser = await user.save();
       return { ...savedUser._doc, password: null, _id: savedUser.id };
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  login: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error('User does not exist!');
+      }
+
+      const doesPassMatch = await bcrypt.compare(password, user.password);
+      if (!doesPassMatch) {
+        throw new Error('Password is incorrect!');
+      }
+
+      const token = jwt.sign({ userId: user.id, email }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+      });
+      return { userId: user.id, token, tokenExpiration: 1 };
     } catch (err) {
       throw err;
     }
